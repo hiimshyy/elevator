ebc5d8c49c963b7dac9ea90e402d899032044726
hiimshyy <caotiensy2k3@gmail.com>
2026-06-05 10:14:04 +0700

    fix: uncommited configPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import { AppShell } from "./components/layout/AppShell";
 import { AlertsMaintenancePage } from "./pages/AlertsMaintenancePage";
-// import { ConfigPage } from "./pages/ConfigPage";
+import { ConfigPage } from "./pages/ConfigPage";
 import { FleetOverviewPage } from "./pages/FleetOverviewPage";
 import { LiveMonitorPage } from "./pages/LiveMonitorPage";
 
@@ -14,7 +14,7 @@ export default function App(): JSX.Element {
         <Route path="/fleet" element={<FleetOverviewPage />} />
         <Route path="/live" element={<LiveMonitorPage />} />
         <Route path="/alerts" element={<AlertsMaintenancePage />} />
-        {/* <Route path="/config" element={<ConfigPage />} /> */}
+        <Route path="/config" element={<ConfigPage />} />
       </Route>
     </Routes>
   );
